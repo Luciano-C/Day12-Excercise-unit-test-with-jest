c6519dcbed756b078a55fabe243fe507fe638ede
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-03-14 12:00:46 -0300

    Borrados comentarios innecesarios

--- test.js
+++ test.js
@@ -31,11 +31,11 @@ test("One dollar should be 106.58 yen", function(){
     // use the function like its suppoed to be used
     const dollars = fromDollarToYen(1)
 
-    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    
     const expected = 1 * 127.9 / 1.2; 
     
     // this is the comparison for the unit test
-     expect(dollars).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(dollars).toBe(expected); 
 })
 
 test("One yen should be 6.3E-3 pounds", function(){
@@ -45,9 +45,9 @@ test("One yen should be 6.3E-3 pounds", function(){
     // use the function like its suppoed to be used
     const dollars = fromYenToPound(1)
 
-    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    
     const expected = 1 * 0.8 / 127.9; 
     
     // this is the comparison for the unit test
-     expect(dollars).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(dollars).toBe(expected); 
 })
